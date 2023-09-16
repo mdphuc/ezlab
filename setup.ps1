95166d4b75b5fa260863b232137043b76c807be7
unknown <mdphuc1110@gmail.com>
2023-09-16 17:37:11 -0400

    add remove function

--- setup.ps1
+++ setup.ps1
@@ -7,7 +7,7 @@ $username = $env:USERNAME
 
 cd "$($drive)\Program Files\Oracle\VirtualBox"
 
-if($setting -ne "vm" -And $setting -ne "os" -And $setting -ne "intnet" -And $setting -ne "Vuln" -And $setting -ne "natnet"){
+if($setting -ne "vm" -And $setting -ne "os" -And $setting -ne "intnet" -And $setting -ne "Vuln" -And $setting -ne "natnet" -And $setting -ne "remove_machine" -And $setting -ne "remove_lab"){
     Write-Host "
 ./setup.ps1
 
@@ -23,7 +23,8 @@ if($setting -ne "vm" -And $setting -ne "os" -And $setting -ne "intnet" -And $set
     vm : setup VM
     vuln : add Vulnerable VM to your environment
     intnet: setup internal network
-    natnet: attach VM to NAT
+    remove_machine: remove a machine
+    remove_lab: remove a lab
     "
 }elseif($setting -eq "os"){
     Invoke-Command{.\VBoxManage list ostypes} | ForEach-Object -Process {Write-Host $_}
@@ -34,7 +35,7 @@ if($setting -ne "vm" -And $setting -ne "os" -And $setting -ne "intnet" -And $set
     $vuln = New-Object System.Collections.ArrayList
     $lab_number = Read-Host "Lab number"
     $machine = @{"VM"=$vm;"Vuln"=$vuln}
-    $jsonBase.Add("$($lab_num)",$machine)
+    $jsonBase.Add("$($lab_number)",$machine)
     $Json = Get-Content -Raw "$($location)\lab.json" | ConvertFrom-Json
 
     $Name = Read-Host "Name"
@@ -73,16 +74,23 @@ if($setting -ne "vm" -And $setting -ne "os" -And $setting -ne "intnet" -And $set
             Invoke-Command {.\VBoxManage storageattach $Name --storagectl "SATA Controller $($VMNum)" --port 0 --device 0 --type hdd --medium "$($env:USERPROFILE)\VirtualBox VMs\$($Name)\$($Name).vdi"} 
             Invoke-Command {.\VBoxManage storagectl $Name --name "IDE Controller $($VMNum)" --add ide}
             Invoke-Command {.\VBoxManage storageattach $Name --storagectl "IDE Controller $($VMNum)" --port 0 --device 0 --type dvddrive --medium $MediaPath}
+            try{
+                $NetworkName = "IsolatedNetwork$($lab_number)"
+                Invoke-Command {.\VBoxManage dhcpserver add --network=$NetworkName --server-ip=10.38.1.1 --lower-ip=10.38.1.10 --upper-ip=10.38.1.140 --netmask=255.255.255.0 --enable}
+            }catch{
+                Write-Host $_
+            }
             Invoke-Command {.\VBoxManage modifyvm $Name --nic1 intnet}
             Invoke-Command {.\VBoxManage modifyvm $Name --intnet1 "IsolatedNetwork$($lab_number)"}
+            Invoke-Command {.\VBoxManage modifyvm $Name --nic2 nat}
         }catch{
             Write-Host $_
         }
         if($Json.$lab_number -eq $null){
             $jsonBase | ConvertTo-Json -Depth 10 | Out-File "$($location)\lab.json"
-            $Json.$lab_number.VM += "$($Name)"
+            $Json.$lab_number."VM" += "$($Name)"
         }else{
-            $Json.$lab_number.VM += "$($Name)"
+            $Json.$lab_number."VM" += "$($Name)"
             $Json | ConvertTo-Json -Depth 10 | Out-File "$($location)\lab.json"
         }
         Write-Host "Successfully Created virtual machine named $($Name)"
@@ -92,9 +100,6 @@ if($setting -ne "vm" -And $setting -ne "os" -And $setting -ne "intnet" -And $set
     $lab_number = Read-Host "Lab number"
     $NetworkName = "IsolatedNetwork$($lab_number)"
     Invoke-Command {.\VBoxManage dhcpserver add --network=$NetworkName --server-ip=10.38.1.1 --lower-ip=10.38.1.10 --upper-ip=10.38.1.140 --netmask=255.255.255.0 --enable}
-}elseif($setting -eq "natnet"){
-    $Name = Read-Host "VM"
-    Invoke-Command {.\VBoxManage modifyvm $Name --nic1 nat}
 }elseif($setting -eq "Vuln"){
     $jsonBase = @{}
     $list = New-Object System.Collections.ArrayList
@@ -142,6 +147,12 @@ if($setting -ne "vm" -And $setting -ne "os" -And $setting -ne "intnet" -And $set
             Invoke-Command {.\VBoxManage storageattach $Name --storagectl "SATA Controller $($VMNum)" --port 0 --device 0 --type hdd --medium "$($env:USERPROFILE)\VirtualBox VMs\$($Name)\$($Name).vdi"} 
             Invoke-Command {.\VBoxManage storagectl $Name --name "IDE Controller $($VMNum)" --add ide}
             Invoke-Command {.\VBoxManage storageattach $Name --storagectl "IDE Controller $($VMNum)" --port 0 --device 0 --type dvddrive --medium $MediaPath}
+            try{
+                $NetworkName = "IsolatedNetwork$($lab_number)"
+                Invoke-Command {.\VBoxManage dhcpserver add --network=$NetworkName --server-ip=10.38.1.1 --lower-ip=10.38.1.10 --upper-ip=10.38.1.140 --netmask=255.255.255.0 --enable}
+            }catch{
+                Write-Host $_
+            }
             Invoke-Command {.\VBoxManage modifyvm $Name --nic1 intnet}
             Invoke-Command {.\VBoxManage modifyvm $Name --intnet1 "IsolatedNetwork$($lab_number)"}
         }catch{
@@ -149,13 +160,25 @@ if($setting -ne "vm" -And $setting -ne "os" -And $setting -ne "intnet" -And $set
         }
         if($Json.$lab_number -eq $null){
             $jsonBase | ConvertTo-Json -Depth 10 | Out-File "$($location)\lab.json"
-            $Json.$lab_number.VM += "$($Name)"
+            $Json.$lab_number."VM" += "$($Name)"
         }else{
-            $Json.$lab_number.VM += "$($Name)"
+            $Json.$lab_number."VM" += "$($Name)"
             $Json | ConvertTo-Json -Depth 10 | Out-File "$($location)\lab.json"
         }
         Write-Host "Successfully Created vuln machine named $($Name)"
     }
+}elseif($setting -eq "remove_machine"){
+    $lab_number = Read-Host "Lab number"
+    $Name = Read-Host "Name"
+    $Json = Get-Content -Raw "$($location)\lab.json" | ConvertFrom-Json
+    try{
+        Invoke-Command {.\VBoxManage unregistervm $Name --delete}
+        $Json.$lab_number."VM" -= $Name
+        $Json | ConvertTo-Json -Depth 10 | Out-File "$($location)\lab.json"
+    }catch{
+        Write-Host $_
+    }
+    
 }
 
 cd "$($location)"
