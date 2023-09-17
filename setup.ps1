307b396ad7a85146e30fa3ea8718659b382bd982
unknown <mdphuc1110@gmail.com>
2023-09-17 15:31:49 -0400

    Add clone, remove_lab, upgrade functionality

--- setup.ps1
+++ setup.ps1
@@ -7,7 +7,7 @@ $username = $env:USERNAME
 
 cd "$($drive)\Program Files\Oracle\VirtualBox"
 
-if($setting -ne "vm" -And $setting -ne "os" -And $setting -ne "intnet" -And $setting -ne "Vuln" -And $setting -ne "natnet" -And $setting -ne "remove_machine" -And $setting -ne "remove_lab"){
+if($setting -ne "vm" -And $setting -ne "os" -And $setting -ne "intnet" -And $setting -ne "Vuln" -And $setting -ne "natnet" -And $setting -ne "remove_machine" -And $setting -ne "remove_lab" -And $setting -ne "clone" -And $setting -ne "upgrade"){
     Write-Host "
 ./setup.ps1
 
@@ -25,6 +25,8 @@ if($setting -ne "vm" -And $setting -ne "os" -And $setting -ne "intnet" -And $set
     intnet: setup internal network
     remove_machine: remove a machine
     remove_lab: remove a lab
+    clone: clone a machine
+    upgrade: upgrade version of a machine
     "
 }elseif($setting -eq "os"){
     Invoke-Command{.\VBoxManage list ostypes} | ForEach-Object -Process {Write-Host $_}
@@ -67,6 +69,7 @@ if($setting -ne "vm" -And $setting -ne "os" -And $setting -ne "intnet" -And $set
         $VMNum = $(Invoke-Command {.\VBoxManage list vms}).Length
         try{
             Invoke-Command {.\VBoxManage createvm --name $Name --ostype $OSType --register}
+            Invoke-Command {.\VBoxManage modifyvm $Name --groups "/Lab$($lab_number)/VM"}
             Invoke-Command {.\VBoxManage modifyvm $Name --cpus $CPU --memory $RAM --vram $VRAM} 
             Invoke-Command {.\VBoxManage modifyvm $Name --graphicscontroller vmsvga}
             Invoke-Command {.\VBoxManage createhd --filename "$($env:USERPROFILE)\VirtualBox VMs\$($Name)\$($Name).vdi" --size $Size --variant Standard}
@@ -78,7 +81,7 @@ if($setting -ne "vm" -And $setting -ne "os" -And $setting -ne "intnet" -And $set
                 $NetworkName = "IsolatedNetwork$($lab_number)"
                 Invoke-Command {.\VBoxManage dhcpserver add --network=$NetworkName --server-ip=10.38.1.1 --lower-ip=10.38.1.10 --upper-ip=10.38.1.140 --netmask=255.255.255.0 --enable}
             }catch{
-                Write-Host $_
+                $ErrorActionPreference = "Continue"
             }
             Invoke-Command {.\VBoxManage modifyvm $Name --nic1 intnet}
             Invoke-Command {.\VBoxManage modifyvm $Name --intnet1 "IsolatedNetwork$($lab_number)"}
@@ -88,9 +91,9 @@ if($setting -ne "vm" -And $setting -ne "os" -And $setting -ne "intnet" -And $set
         }
         if($Json.$lab_number -eq $null){
             $jsonBase | ConvertTo-Json -Depth 10 | Out-File "$($location)\lab.json"
-            $Json.$lab_number."VM" += "$($Name)"
+            $Json.$lab_number.VM += "$($Name)"
         }else{
-            $Json.$lab_number."VM" += "$($Name)"
+            $Json.$lab_number.VM += "$($Name)"
             $Json | ConvertTo-Json -Depth 10 | Out-File "$($location)\lab.json"
         }
         Write-Host "Successfully Created virtual machine named $($Name)"
@@ -140,6 +143,7 @@ if($setting -ne "vm" -And $setting -ne "os" -And $setting -ne "intnet" -And $set
         $VMNum = $(Invoke-Command {.\VBoxManage list vms}).Length
         try{
             Invoke-Command {.\VBoxManage createvm --name $Name --ostype $OSType --register}
+            Invoke-Command {.\VBoxManage modifyvm --name $Name --groups "/Lab$($lab_number)/Vuln"}
             Invoke-Command {.\VBoxManage modifyvm $Name --cpus $CPU --memory $RAM --vram $VRAM} 
             Invoke-Command {.\VBoxManage modifyvm $Name --graphicscontroller vmsvga}
             Invoke-Command {.\VBoxManage createhd --filename "$($env:USERPROFILE)\VirtualBox VMs\$($Name)\$($Name).vdi" --size $Size --variant Standard}
@@ -151,7 +155,7 @@ if($setting -ne "vm" -And $setting -ne "os" -And $setting -ne "intnet" -And $set
                 $NetworkName = "IsolatedNetwork$($lab_number)"
                 Invoke-Command {.\VBoxManage dhcpserver add --network=$NetworkName --server-ip=10.38.1.1 --lower-ip=10.38.1.10 --upper-ip=10.38.1.140 --netmask=255.255.255.0 --enable}
             }catch{
-                Write-Host $_
+                $ErrorActionPreference = "Continue"
             }
             Invoke-Command {.\VBoxManage modifyvm $Name --nic1 intnet}
             Invoke-Command {.\VBoxManage modifyvm $Name --intnet1 "IsolatedNetwork$($lab_number)"}
@@ -160,9 +164,9 @@ if($setting -ne "vm" -And $setting -ne "os" -And $setting -ne "intnet" -And $set
         }
         if($Json.$lab_number -eq $null){
             $jsonBase | ConvertTo-Json -Depth 10 | Out-File "$($location)\lab.json"
-            $Json.$lab_number."VM" += "$($Name)"
+            $Json.$lab_number.VM += "$($Name)"
         }else{
-            $Json.$lab_number."VM" += "$($Name)"
+            $Json.$lab_number.VM += "$($Name)"
             $Json | ConvertTo-Json -Depth 10 | Out-File "$($location)\lab.json"
         }
         Write-Host "Successfully Created vuln machine named $($Name)"
@@ -172,6 +176,7 @@ if($setting -ne "vm" -And $setting -ne "os" -And $setting -ne "intnet" -And $set
     $Name = Read-Host "Name"
     $Json = Get-Content -Raw "$($location)\lab.json" | ConvertFrom-Json
     try{
+        Invoke-Command {.\VBoxManage modifyvm --name $Name --groups ""}
         Invoke-Command {.\VBoxManage unregistervm $Name --delete}
         $Json.$lab_number."VM" -= $Name
         $Json | ConvertTo-Json -Depth 10 | Out-File "$($location)\lab.json"
@@ -179,6 +184,52 @@ if($setting -ne "vm" -And $setting -ne "os" -And $setting -ne "intnet" -And $set
         Write-Host $_
     }
     
+}elseif($setting -eq "remove_lab"){
+    $lab_number = Read-Host "Lab number"
+    $Json = Get-Content -Raw "$($location)\lab.json" | ConvertFrom-Json
+    ForEach($machine in $Json.$lab_number."VM"){
+        try{
+            Invoke-Command {.\VBoxManage modifyvm --name $machine --groups ""}
+            Invoke-Command {.\VBoxManage unregistervm $machine --delete}
+        }catch{
+            Write-Host $_
+        }
+    }
+    try{
+        $Json.$lab_number = $Json.$lab_number | Select-Object "VM" -ExcludeProperty "VM"
+        $Json.$lab_number = $Json.$lab_number | Select-Object "Vuln" -ExcludeProperty "Vuln"
+        $Json | ConvertTo-Json -Depth 10 | Out-File "$($location)\lab.json"
+    }catch{
+        Write-Host $_
+    }
+    
+}elseif($setting -eq "clone"){
+    $Name = Read-Host "Name of the machine you want to clone"
+    $Name_clone = Read-Host "Name of new machine"
+    try{
+        Invoke-Command {.\VBoxManage clonevm $Name --name $Name_clone --register --mode=all}
+        Write-Host "Successfully clone $($Name)"
+    }catch{
+        Write-Host $_
+    }
+}elseif($setting -eq "upgrade"){
+    $VMNum = $(Invoke-Command {.\VBoxManage list vms}).Length
+    $Name = Read-Host "Name"
+    $Path = Read-Host "Path to the later version image file"
+    for ($i = 1; $i -lt $VMNum; $i++){
+        try{
+            Invoke-Command {.\VBoxManage storageattach $Name --storagectl "IDE Controller $($i)" --port 0 --device 0 --type dvddrive --forceunmount --medium none}
+        }catch{
+            $ErrorActionPreference = "Continue"
+        }
+    }
+    for ($i = 1; $i -lt $VMNum; $i++){
+        try{
+            Invoke-Command {.\VBoxManage storageattach $Name --storagectl "IDE Controller $($i)" --port 0 --device 0 --type dvddrive --forceunmount --medium $Path}
+        }catch{
+            $ErrorActionPreference = "Continue"
+        }
+    }
 }
 
 cd "$($location)"
