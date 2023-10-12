78cce5076163a9de68f6aa07767a8cdee1977cb2
unknown <mdphuc1110@gmail.com>
2023-10-12 10:50:02 -0400

    fix ip

--- setup.ps1
+++ setup.ps1
@@ -79,7 +79,7 @@ if($setting -ne "vm" -And $setting -ne "os" -And $setting -ne "intnet" -And $set
             Invoke-Command {.\VBoxManage.exe unattended install $Name --iso=$MediaPath --user=$username --password=$password}
             try{
                 $NetworkName = "IsolatedNetwork$($lab_number)"
-                Invoke-Command {.\VBoxManage.exe dhcpserver add --network=$NetworkName --server-ip=10.38.$(lab_number).1 --lower-ip=10.38.$(lab_number).10 --upper-ip=10.38.$(lab_number).140 --netmask=255.255.255.0 --enable}
+                Invoke-Command {.\VBoxManage.exe dhcpserver add --network=$NetworkName --server-ip="10.38.$($lab_number).1" --lower-ip="10.38.$($lab_number).10" --upper-ip="10.38.$($lab_number).140" --netmask=255.255.255.0 --enable}
             }catch{
                 $ErrorActionPreference = "Continue"
             }
@@ -167,7 +167,7 @@ if($setting -ne "vm" -And $setting -ne "os" -And $setting -ne "intnet" -And $set
             Invoke-Command {.\VBoxManage.exe storageattach $Name --storagectl "IDE Controller $($VMNum)" --port 0 --device 0 --type dvddrive --medium $MediaPath}
             try{
                 $NetworkName = "IsolatedNetwork$($lab_number)"
-                Invoke-Command {.\VBoxManage.exe dhcpserver add --network=$NetworkName --server-ip=10.38.$(lab_number).1 --lower-ip=10.38.$(lab_number).10 --upper-ip=10.38.$(lab_number).140 --netmask=255.255.255.0 --enable}
+                Invoke-Command {.\VBoxManage.exe dhcpserver add --network=$NetworkName --server-ip="10.38.$($lab_number).1" --lower-ip="10.38.$($lab_number).10" --upper-ip="10.38.$($lab_number).140" --netmask=255.255.255.0 --enable}
             }catch{
                 $ErrorActionPreference = "Continue"
             }
@@ -213,7 +213,7 @@ if($setting -ne "vm" -And $setting -ne "os" -And $setting -ne "intnet" -And $set
     try{
         Invoke-Command {.\VBoxManage.exe clonevm $Name --name $Name_clone --register --mode=all}
         Invoke-Command {.\VBoxManage.exe modifyvm $Name_clone --groups "/Lab$($lab_number)/VM"}
-        Invoke-Command {.\VBoxManage.exe dhcpserver add --network=$NetworkName --server-ip=10.38.$(lab_number).1 --lower-ip=10.38.$(lab_number).10 --upper-ip=10.38.$(lab_number).140 --netmask=255.255.255.0 --enable}
+        Invoke-Command {.\VBoxManage.exe dhcpserver add --network=$NetworkName --server-ip="10.38.$($lab_number).1" --lower-ip="10.38.$($lab_number).10" --upper-ip="10.38.$($lab_number).140" --netmask=255.255.255.0 --enable}
 
         Invoke-Command {.\VBoxManage.exe modifyvm $Name_clone --intnet1 "IsolatedNetwork$($lab_number)"}
         Write-Host "Successfully clone $($Name)"
@@ -262,7 +262,7 @@ if($setting -ne "vm" -And $setting -ne "os" -And $setting -ne "intnet" -And $set
                 Invoke-Command {.\VBoxManage.exe modifyvm $Name --groups "/Lab$($lab_number)/$($Type)"}
                 try{
                     $NetworkName = "IsolatedNetwork$($lab_number)"
-                    Invoke-Command {.\VBoxManage.exe dhcpserver add --network=$NetworkName --server-ip=10.38.$(lab_number).1 --lower-ip=10.38.$(lab_number).10 --upper-ip=10.38.$(lab_number).140 --netmask=255.255.255.0 --enable}
+                    Invoke-Command {.\VBoxManage.exe dhcpserver add --network=$NetworkName --server-ip="10.38.$($lab_number).1" --lower-ip=10.38.$(lab_number).10 --upper-ip=10.38.$(lab_number).140 --netmask=255.255.255.0 --enable}
                 }catch{
                     $ErrorActionPreference = "Continue"
                 }
